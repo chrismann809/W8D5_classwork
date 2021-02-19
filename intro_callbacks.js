77e46fffe58343879ecfb6d8845275b10f2314b9
Andrew Kim <andrewikim95@gmail.com>
2021-02-19 15:14:28 -0500

    finished #addNumbers

--- intro_callbacks.js
+++ intro_callbacks.js
@@ -34,3 +34,24 @@ class Clock {
 
 // const c = new Clock();
 
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if(numsLeft === 0){
+        completionCallback(sum);
+    }
+    else {
+        reader.question('Please enter a number: ', function(numInput) {
+            let num = parseInt(numInput);
+            sum += num;
+            console.log(`Sum so far: ${sum}`);
+            addNumbers(sum, numsLeft-1, completionCallback);
+        });
+    }
+};
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
